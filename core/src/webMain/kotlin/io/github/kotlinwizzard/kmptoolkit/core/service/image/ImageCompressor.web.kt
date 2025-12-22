f439847b33b7dc5b2a03228f42e09156cec5692d
Kräutl Philip <philip.kraeutl@gmail.com>
2025-12-22 13:01:20 +0100

    add image compress for web

--- core/src/webMain/kotlin/io/github/kotlinwizzard/kmptoolkit/core/service/image/ImageCompressor.web.kt
+++ core/src/webMain/kotlin/io/github/kotlinwizzard/kmptoolkit/core/service/image/ImageCompressor.web.kt
@@ -1,11 +1,17 @@
 package io.github.kotlinwizzard.kmptoolkit.core.service.image
 
-import org.jetbrains.skia.ImageInfo
-import kotlin.js.ExperimentalWasmJsInterop
+import org.jetbrains.skia.EncodedImageFormat
+import org.jetbrains.skia.Image
 
 actual fun ImageCompressor.compressImage(
     content: ByteArray,
     compressionRatio: Float
 ): ByteArray {
-   return content
+
+    val image = Image.makeFromEncoded(content)
+    val data = image.encodeToData(
+        EncodedImageFormat.JPEG,
+        (compressionRatio*100).toInt().coerceIn(0,100)
+    ) ?: error("Failed to encode image")
+    return data.bytes
 }
